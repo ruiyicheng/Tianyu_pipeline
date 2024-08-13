9be9bef2b90b0ffdc1828279d15d8fc5543c83b6
ruiyicheng <1604685779@qq.com>
2024-08-13 20:19:21 +0800

    update psg

--- pipeline/utils/process_site_getter.py
+++ pipeline/utils/process_site_getter.py
@@ -13,9 +13,10 @@ class process_site_getter:
             sql = "SELECT * FROM data_process_site;"
             args = tuple()
             res = self.sql_interface.query(sql,args)
-            ip_this = set(res['process_site_ip'])&set(local_host_list)
-            assert len(ip_this)==1
-            ret = res[res['process_site_ip']==list(ip_this)[0]]
+            #ip_this = set(res['process_site_ip'])&set(local_host_list)
+            #assert len(ip_this)==1
+            ip_this = (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [[(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0]
+            ret = res[res['process_site_ip']==ip_this]
         else:
             sql = "SELECT * FROM data_process_site where process_site_id = %s;"
             args = (id_channel,)
