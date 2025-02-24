6c44a292d5b601ef19fffecf7e2904eb1f0180cf
ruiyicheng <1604685779@qq.com>
2025-02-24 23:13:01 +0800

    add monitor to system

---
+++ testbench/monitor_python.sh
@@ -0,0 +1,28 @@
+# Monitor the resource usage of python processes
+ext=0
+trap 'echo logged off at $(date +"%Y-%m-%dT%H:%M:%S%z"); exit; ext=1' INT
+echo "PID,cpu,memory,time,process_name,disk_read_kb,disk_write_kb,time" > "monitor$$.csv"
+while true
+    do
+    cpu_and_mem=$(top -n 1 -b | sed '1,7d' | sed 's/^  *//g' | sed 's/  */ /g' | cut -f1,9,10-12 -d' ')
+    mysql_cpu=$(echo "$cpu_and_mem"| grep 'mysqld')
+    python_cpu=$(echo "$cpu_and_mem"| grep 'python')
+    all_cpu_mem="$mysql_cpu"$'\n'$"$python_cpu"
+    while IFS= read -r line; do
+        #echo "Processing line: $line"
+        # Add your processing logic here
+        python_pid=$(echo "$line" | cut -d' ' -f1)
+        #echo $python_pid
+        time_now=$(date +"%Y-%m-%dT%H:%M:%S%z")
+        pydisk="$(sudo iotop -b -p "$python_pid" -n 1 -k -P -qqq | sed 's/  */ /g' | sed 's/^ *//g'| cut -d' ' -f4,6)"
+        #echo $pydisk
+        final_line=$(echo "$line $pydisk")
+        echo $final_line $time_now | sed 's/ /,/g' >> "monitor$$.csv"
+    done <<< "$all_cpu_mem"
+    if [ $ext -eq 1 ]
+    then
+        echo 'Bye'
+        exit
+    fi
+done
+# mysqld_pid=
