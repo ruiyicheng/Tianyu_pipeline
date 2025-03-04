3cc39f496f89772d37cbe3a3da7bf05ecf8849d4
ruiyicheng <1604685779@qq.com>
2025-03-04 18:26:56 +0800

    revise monitor

--- testbench/monitor_python.sh
+++ testbench/monitor_python.sh
@@ -1,22 +1,22 @@
 # Monitor the resource usage of python processes
 trap 'echo logged off at $(date +"%Y-%m-%dT%H:%M:%S%z"); exit;' INT
-echo "PID,cpu,memory,time,process_name,disk_read_kb,disk_write_kb,time" > "monitor$$.csv"
+echo "Starting monitor: PID=$$"
+echo "PID,cpu,memory,kB_rd/s,kB_wr/s,kB_ccwr/s,cmdname,time" > "monitor$$.csv"
 while true
     do
     cpu_and_mem=$(top -n 1 -b | sed '1,7d' | sed 's/^  *//g' | sed 's/  */ /g' | cut -f1,9,10-12 -d' ')
     mysql_cpu=$(echo "$cpu_and_mem"| grep 'mysqld')
     python_cpu=$(echo "$cpu_and_mem"| grep 'python')
     all_cpu_mem="$mysql_cpu"$'\n'$"$python_cpu"
-    while IFS= read -r line; do
-        #echo "Processing line: $line"
-        # Add your processing logic here
-        python_pid=$(echo "$line" | cut -d' ' -f1)
-        #echo $python_pid
-        time_now=$(date +"%Y-%m-%dT%H:%M:%S")
-        pydisk="$(sudo iotop -b -p "$python_pid" -n 1 -k -P -qqq | sed 's/  */ /g' | sed 's/^ *//g'| cut -d' ' -f4,6)"
-        #echo $pydisk
-        final_line=$(echo "$line $pydisk")
-        echo $final_line $time_now | sed 's/ /,/g' >> "monitor$$.csv"
-    done <<< "$all_cpu_mem"
+    python_pid=$(echo "$all_cpu_mem" | cut -d' ' -f1 | tr '\n' ',')
+    #echo $python_pid
+    time_now=$(date +"%Y-%m-%dT%H:%M:%S")
+    pydisk="$(sudo pidstat -p "$python_pid" -h -r -u -d 1 1 |  sed '1,3d' | sed 's/  */ /g' | cut -d' ' -f4,9,15-18,20 | tr ' ' ',')"
+    #echo "$pydisk"
+    
+    # duplicate time_now to the same lines as pydisk
+    num_lines=$(echo "$pydisk" | wc -l)
+    time_now=$(yes "$time_now" | head -n "$num_lines")
+    paste <(echo "$pydisk") <(echo "$time_now") -d',' | tr -d "^ "  >> "monitor$$.csv"
 done
 # mysqld_pid=
